1ee1547dba2231b7476fe2aa45308246ac7fb040
Alexandre De Spiegeleer <alex-ds@live.be>
2022-06-11 17:25:53 +0200

    Added more placeholders

--- trainer.py
+++ trainer.py
@@ -10,7 +10,7 @@ from dataset import train_dataset
 from dataset import test_dataset as val_dataset
 
 
-class plModel(pl.LightningModule):
+class PLModel(pl.LightningModule):
     def __init__(self):
         super(plModel, self).__init__()
 
@@ -20,7 +20,6 @@ class plModel(pl.LightningModule):
     def forward(self, x):
         out = self.model(x)
         return out
-
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.model.parameters(), lr=1e-3)
@@ -41,10 +40,56 @@ class plModel(pl.LightningModule):
         return loss
 
 
+class DataModule(pl.LightningDataModule):
+    """
+    https://pytorch-lightning.readthedocs.io/en/latest/data/datamodule.html
+    """
+    def __init__(self, batch_size=32):
+        self.batch_size = batch_size
+
+    def prepare_data(self):
+        """
+        Run single process on CPU
+        """
+        # Download, tokenize, ...
+        pass
+
+    def setup(self):
+        """
+        Operations on every GPU
+        """
+        # split, transform, ...
+
+        #self.train =
+        #self.val =
+        #self.test =
+        pass
+
+    def train_dataloader(self):
+        train = DataLoader(self.train, batch_size=self.batch_size)
+        return train
+
+    def val_dataloader(self):
+        val = DataLoader(self.val, batch_size=self.batch_size)
+        return val
+
+    def test_dataloader(self):
+        test = DataLoader(self.test, batch_size=self.batch_size)
+        return test
+
+
 
 if __name__=='__main__':
+    # https://pytorch-lightning.readthedocs.io/en/stable/common/early_stopping.html
+    # https://pytorch-lightning.readthedocs.io/en/stable/common/loggers.html
+    # https://pytorch-lightning.readthedocs.io/en/stable/advanced/training_tricks.html#batch-size-finder
+    # https://pytorch-lightning.readthedocs.io/en/stable/common/lightning_module.html#lightning-hooks
+    # https://pytorch-lightning.readthedocs.io/en/latest/common/optimization.html#id5
+    # https://pytorch-lightning.readthedocs.io/en/latest/api/pytorch_lightning.callbacks.LearningRateMonitor.html#pytorch_lightning.callbacks.LearningRateMonitor
+    # https://neptune.ai/blog/pytorch-loss-functions
     callbacks = []
-    model = plModel()
+    model = PLModel()
+    dm    = DataModule()
     trainer_params = {
             'gpus': 1
             }
