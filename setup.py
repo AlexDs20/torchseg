f85d863fa2b5613798e2de512ade504a152d281c
Alexandre De Spiegeleer <alex-ds@live.be>
2022-06-21 22:28:52 +0200

    renamed package

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
   long_description = f.read()
 
-setup(name='segmentation',
+setup(name='torchseg',
       version='0.0.1',
       description='A library for semantic segmentation',
       author='Alexandre De Spiegeleer',
       license='MIT',
       long_description=long_description,
       long_description_content_type='text/markdown',
-      packages = ['segmentation'],
+      packages = ['torchseg'],
       install_requires=['numpy', 'torch', 'pytorch-lightning', 'pyyaml', 'torchmetrics'])
 
