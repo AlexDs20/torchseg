44cb9cb3acb014e9ae5f3cc1c2802db0f0ddfe27
Alexandre De Spiegeleer <alex-ds@live.be>
2022-09-28 22:04:16 +0200

    Make subtile support 4d

--- test/test_utils.py
+++ test/test_utils.py
@@ -4,7 +4,34 @@ from torchseg.utils import make_subtiles, unmake_subtiles
 
 
 class TestSubtiles():
-    def test_make_subtiles(self):
+    def test_make_subtiles_4d(self):
+        shape = (4, 12, 512, 1024)
+        h, w = 16, 16
+        data = torch.rand(*shape)
+
+        out = make_subtiles(data, h, w)
+        assert out.shape == (shape[0] * (shape[2] // h) * (shape[3] // w), shape[1], h, w)
+
+    def test_unmake_subtiles_4d(self):
+        b, m, n = 4, 512, 1024
+        c, h, w = 13, 128, 128
+        shape = (b * (m // h) * (n // w), c, h, w)
+        data = torch.rand(*shape)
+
+        out = unmake_subtiles(data, m, n)
+        assert out.shape == (b, c, m, n)
+
+    def test_make_unmake_id_4d(self):
+        h, w = 16, 16
+        b, c, m, n = (4, 13, 512, 1024)
+        shape = (b, c, m, n)
+        data = torch.rand(*shape)
+
+        out = make_subtiles(data, h, w)
+        out = unmake_subtiles(out, m, n)
+        assert torch.all(out == data)
+
+    def test_make_subtiles_3d(self):
         shape = (12, 512, 1024)
         h, w = 16, 16
         data = torch.rand(*shape)
@@ -12,16 +39,17 @@ class TestSubtiles():
         out = make_subtiles(data, h, w)
         assert out.shape == ((shape[1] // h) * (shape[2] // w), shape[0], h, w)
 
-    def test_unmake_subtiles(self):
+    def test_unmake_subtiles_3d(self):
         m, n = 512, 1024
         c, h, w = 13, 128, 128
         shape = ((m // h) * (n // w), c, h, w)
         data = torch.rand(*shape)
 
         out = unmake_subtiles(data, m, n)
+        out = out[0]
         assert out.shape == (c, m, n)
 
-    def test_make_unmake_id(self):
+    def test_make_unmake_id_3d(self):
         h, w = 16, 16
         c, m, n = (13, 512, 1024)
         shape = (c, m, n)
@@ -29,4 +57,6 @@ class TestSubtiles():
 
         out = make_subtiles(data, h, w)
         out = unmake_subtiles(out, m, n)
+        out = out[0]
+        assert out.shape == data.shape
         assert torch.all(out == data)
--- torchseg/utils.py
+++ torchseg/utils.py
@@ -11,14 +11,19 @@ def make_subtiles(x: torch.Tensor, h: int, w: int) -> torch.Tensor:
     Output:
      Torch tensor of shape (N,C,H,W) with N = the number of subtiles created.
     """
-    c, m, n = x.shape
+    if x.dim() == 3:
+        x = x.unsqueeze(0)
+    elif x.dim() != 4:
+        raise ValueError(f'Input dimension must be either 3 or 4. {x.dim} dimensions provided')
+
+    b, c, m, n = x.shape
 
     if m // h != m / h:
         raise ValueError(f'Required subtile size of {h} for tensor size of {m}, does not create an exact number of subtiles. Use padding or change subtile size.')
     elif n // w != n / w:
         raise ValueError(f'Required subtile size of {w} for tensor size of {n}, does not create an exact number of subtiles. Use padding or change subtile size.')
 
-    return x.reshape(c, m // h, h, n // w, w).permute(1, 3, 0, 2, 4).reshape(-1, c, h, w)
+    return x.reshape(b, c, m // h, h, n // w, w).permute(2, 4, 0, 1, 3, 5).reshape(-1, c, h, w)
 
 
 def unmake_subtiles(x: torch.Tensor, m: int, n: int) -> torch.Tensor:
@@ -29,11 +34,17 @@ def unmake_subtiles(x: torch.Tensor, m: int, n: int) -> torch.Tensor:
      m: int of the origial height of the image (height to recreate)
      n: into f the original width of the image (width to recreate)
     """
+    if x.dim() == 3:
+        x = x.unsqueeze(0)
+    elif x.dim() != 4:
+        raise ValueError(f'Input dimension must be either 3 or 4. {x.dim} dimensions provided')
+
     c, h, w = x.shape[-3:]
+    b = x.shape[0] // ((m // h) * (n // w))
 
     if m // h != m / h:
         raise ValueError(f'Required larger torch of size {m} while subtiles are of size {h} this does not work.')
     elif n // w != n / w:
         raise ValueError(f'Required larger torch of size {n} while subtiles are of size {w} this does not work.')
 
-    return x.reshape(m // h, n // w, c, h, w).permute(2, 0, 3, 1, 4).reshape(c, m, n)
+    return x.reshape(m // h, n // w, b, c, h, w).permute(2, 3, 0, 4, 1, 5).reshape(b, c, m, n)
