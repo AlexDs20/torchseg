6392f6e86f68ae5223e421335e1fef49462db449
Alexandre De Spiegeleer <alex-ds@live.be>
2022-10-01 20:29:25 +0200

    Some sort of inference code

--- torchseg/utils.py
+++ torchseg/utils.py
@@ -2,14 +2,14 @@ import torch
 
 
 def make_subtiles(x: torch.Tensor, h: int, w: int) -> torch.Tensor:
-    """ Reshape a 3D tensor into a 4D with the new dimension used to index the subtiles
+    """ Reshape a 3D or 4D tensor into a new 4D with the dimension 0 used to index the subtiles (and batch)
     Inputs:
-     x: tensor to split into subtiles of size h, w. Size of x: (C,H,W)
+     x: tensor to split into subtiles of size h, w. Size of x: (C,H,W) or (N,C,H,W)
      h: int indicating the height of the subtile
      w: int indicating the width of the subtile to use
 
     Output:
-     Torch tensor of shape (N,C,H,W) with N = the number of subtiles created.
+     Torch tensor of shape (N,C,H,W) with N = the total number of subtiles created.
     """
     if x.dim() == 3:
         x = x.unsqueeze(0)
@@ -27,7 +27,7 @@ def make_subtiles(x: torch.Tensor, h: int, w: int) -> torch.Tensor:
 
 
 def unmake_subtiles(x: torch.Tensor, m: int, n: int) -> torch.Tensor:
-    """ Reshape the subtiles created by make_subtiles into an tensor of the original size
+    """ Reshape the subtiles created by make_subtiles into a tensor of the original size
 
     Inputs:
      x: torch tensor with 4D with structure (N,C,H,W)
