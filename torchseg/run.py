e059d4400f205fcb8232df9a33105d9fdc1bc59b
Alexandre De Spiegeleer <alex-ds@live.be>
2022-10-01 12:31:15 +0200

    Add support for only valid and test

--- torchseg/run.py
+++ torchseg/run.py
@@ -10,12 +10,13 @@ from torchseg.transfer_learning import transfer_learning
 from .plModel import plModel
 
 
-def train(cfg_file):
+def run(cfg_file):
     with open(cfg_file) as cfg:
         config = yaml.load(cfg, Loader=yaml.Loader)
 
     train_dataloader = DataLoader(FolderDataSet(config['data']['train_folder']), **config['dataloader']['train'])
     valid_dataloader = DataLoader(FolderDataSet(config['data']['valid_folder']), **config['dataloader']['valid'])
+    test_dataloader = DataLoader(FolderDataSet(config['data']['test_folder']), **config['dataloader']['test'])
 
     callbacks = get_callbacks(config['callbacks'])
     loggers = get_loggers(config['loggers'])
@@ -27,7 +28,17 @@ def train(cfg_file):
     if config['transfer_learning'] is not None:
         model = transfer_learning(model, config['transfer_learning'], callbacks, loggers, train_dataloader, valid_dataloader)
 
-    trainer.fit(model, train_dataloader, valid_dataloader, ckpt_path=config['resume_from_ckpt'])
+    if config['run']['train']:
+        trainer.fit(model, train_dataloader, valid_dataloader, ckpt_path=config['resume_from_ckpt'])
+        ckpt_path = None
+    else:
+        ckpt_path = config['resume_from_ckpt']
+
+    if config['run']['valid']:
+        trainer.validate(model, valid_dataloader, ckpt_path=ckpt_path)
+
+    if config['run']['test']:
+        trainer.test(model, test_dataloader, ckpt_path=ckpt_path)
 
 
 if __name__ == '__main__':
@@ -35,4 +46,4 @@ if __name__ == '__main__':
     parser.add_argument('--config', '-c', help='path to the config.yaml file to be used', default='config.yaml')
     args = parser.parse_args()
 
-    train(args.config)
+    run(args.config)
